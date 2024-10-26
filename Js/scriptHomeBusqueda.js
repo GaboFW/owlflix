4f98217a614a0245321aa002a751fda6a5f6ad0d
Gabriel Fernandez Waisberg <gabibo.fewars@gmail.com>
2024-10-26 09:50:03 -0300

    v3.1

--- Js/scriptHomeBusqueda.js
+++ Js/scriptHomeBusqueda.js
@@ -2,7 +2,7 @@ function $(id) {
     return document.getElementById(id);
 }
 
-$("search-button").addEventListener("click", cambiarHref());
+$("search-button").addEventListener("click", cambiarHref);
 
 $("search-bar").addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
