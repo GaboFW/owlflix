11cb11a935c0e381d92eaa23e1aea5e8a09da7ad
Gabriel Fernandez Waisberg <gabibo.fewars@gmail.com>
2024-10-27 21:23:49 -0300

    v.final

--- Js/scriptMensaje.js
+++ Js/scriptMensaje.js
@@ -6,14 +6,11 @@ function mostrarMensaje(e) {
     e.preventDefault();
 
     Swal.fire({
-        position: "bottom-end",
+        position: "center",
         icon: "success",
         title: "¡Se envió correctamente!",
         showConfirmButton: false,
-        timer: 1500,
-        customClass: {
-            popup: 'small-swal'
-        }
+        timer: 1500
     });
 }
 
